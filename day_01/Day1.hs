1b6ead8a36428a7a08b5262859aae3b22a7f8e2f
Felipe Sasdelli <sasdelli.pi@gmail.com>
2022-12-03 17:05:52 -0300

    finish day 01

---
+++ day_01/Day1.hs
@@ -0,0 +1,22 @@
+module Day1 where
+
+import Data.List    (sortBy)
+import Data.Functor ((<&>))
+
+countCalories :: FilePath -> IO [Integer]
+countCalories fileName = do
+    contents <- lines <$> readFile fileName
+    let elfs :: [String] -> [[String]]
+        elfs input = case break (== "") input of
+            ([], _     ) -> []
+            (xs, []    ) -> [xs]
+            (xs, _ : ys) -> xs : elfs ys
+        calories    = map (map (read :: String -> Integer)) (elfs contents)
+        sumCalories = map sum calories
+    return sumCalories
+
+part1 :: IO Integer
+part1 = countCalories "input.txt" <&> maximum
+
+part2 :: IO Integer
+part2 = countCalories "input.txt" <&> (sum . take 3 . sortBy (flip compare))
